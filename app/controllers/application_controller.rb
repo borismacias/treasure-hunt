d453689274410bf30f56ae5ef406f3af6b97ea48
Boris Gonzalez <boris.gonzalez.macias@gmail.com>
2024-02-15 01:10:35 -0300

    chore(auth): adds auth via headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    access_token = request.headers['access-token']
+    @user = User.find_by(access_token:)
+    head(401) if @user.blank?
+  end
 end
