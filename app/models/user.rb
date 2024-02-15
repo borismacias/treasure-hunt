e830d3f08effd1942a99576e07274ff668dea463
Boris Gonzalez <boris.gonzalez.macias@gmail.com>
2024-02-15 08:06:58 -0300

    feat(user): set as winner

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,17 @@ class User < ApplicationRecord
 
   validates_presence_of :name, :email
 
+  def set_as_winner!
+    return if winner
+
+    update(winner: true)
+    notify_user
+  end
+
   private
+
+  def notify_user
+  end
 
   def set_access_token
     access_token = SecureRandom.urlsafe_base64(nil, false)
