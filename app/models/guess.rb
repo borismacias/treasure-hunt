d09412269757ffc0a27a5fd7eb3d43969b471449
Boris Gonzalez <boris.gonzalez.macias@gmail.com>
2024-02-15 07:57:32 -0300

    feat(guess): implements check_for_winner method

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,4 +2,9 @@ class Guess < ApplicationRecord
   belongs_to :user, dependent: :destroy
 
   validates_presence_of :lat, :lng
+
+  def check_winner
+    distance = CalculateDistanceToTarget.for(lat:, lng:)
+    distance < 1000
+  end
 end
