c5308d677c8f4597d604e7627fda8b352db3f255
Boris Gonzalez <boris.gonzalez.macias@gmail.com>
2024-02-15 07:55:13 -0300

    fix(calculate-distance): update wrong name

--- app/commands/calculate_distance_to_target.rb
+++ app/commands/calculate_distance_to_target.rb
@@ -1,9 +1,9 @@
 class CalculateDistanceToTarget
   class << self
-    def for(lat, lng) # rubocop:disable Metrics/AbcSize
+    EARTH_RADIUS_METERS = 6371 * 1000
+
+    def for(lat:, lng:) # rubocop:disable Metrics/AbcSize
       rad_per_deg = Math::PI / 180
-      earth_radius_km = 6371
-      rm = earth_radius_km * 1000
 
       dlat_rad = (lat - target_lat) * rad_per_deg
       dlng_rad = (lng - target_lng) * rad_per_deg
@@ -14,7 +14,7 @@ class CalculateDistanceToTarget
       a = Math.sin(dlat_rad / 2)**2 + Math.cos(target_lat_rad) * Math.cos(lat_rad) * Math.sin(dlng_rad / 2)**2
       c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
 
-      rm * c
+      EARTH_RADIUS_METERS * c
     end
 
     def target_lat
